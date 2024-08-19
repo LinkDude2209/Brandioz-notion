7247d3498c86f07069c282dbccc470444961310c
Rudro Bhattacharya <rudrobhattacharya@Rudros-MacBook-Air.local>
2024-08-20 00:28:39 +0530

    fixed next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 export default {
-    experimental: {
-        serverActions: true, // Assuming you need Server Actions
-    },
     images: {
         domains: ['images.unsplash.com', 'assets.aceternity.com', 'tlsgdkhnoysuuwtijxjk.supabase.co'],
     },
